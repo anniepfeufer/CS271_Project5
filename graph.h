ff847f5fd6c287f03492d071448082e21afe1014
margaretmalan <122831330+margaretmalan@users.noreply.github.com>
2023-03-31 14:17:09 -0400

    Add files via upload

---
+++ graph.h
@@ -0,0 +1,27 @@
+//==================================
+// graph.h
+//==================================
+
+#include <iostream>
+using namespace std;
+
+//==================================
+// Graph Class
+//==================================
+template <typename D, typename K>
+class Graph
+{
+private:
+	struct Node //node structure 
+	{
+		D vertex;
+		K key;
+		Node *next;
+	}
+
+	D* V; //dynamic array of vertices 
+	Node* Adj; //dynamic array of pointer to nodes 
+
+public:
+	
+}
