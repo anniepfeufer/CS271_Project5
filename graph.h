8f66f692a3df38099cf79b26d8f6e7b44c950371
anniepfeufer <pfeufe_a1@denison.edu>
2023-04-03 14:32:03 -0400

    Update graph.h

--- graph.h
+++ graph.h
@@ -14,16 +14,26 @@ class Graph
 private:
 	struct Node //node structure 
 	{
-		D vertex;
+		D data;
 		K key;
-		Node *next;
+		
 	}
 
-	D* V; //dynamic array of vertices 
-	Node* Adj; //dynamic array of pointer to nodes 
+	Node* V; //dynamic array of vertices 
+	int size; //number of verticies in the graph
+	vector<K>* Adj; //dynamic array of pointer to nodes 
 
 public:
 	Graph(vector<K> keys, vector<D> data, vector<vector<K>> edges){
-		
+		size=size(keys);
+		V= new Node<D,K>[size]
+		Adj=new vector<K>[size]
+		for (int i=0; i<size; i++){
+			Node<D,K> curr;
+			curr.data=data.at(i);
+			curr.key=keys.at(i);
+			V[i]=curr;
+			Adj[i]
+		}
 	}
-}
+};
