0bbadf7c1709124ff4b06f6431612b0d5a257c01
Isabella8282 <deassi_i1@denison.edu>
2023-04-04 12:26:12 -0400

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -14,7 +14,24 @@ int main(void)
     data.push_back(4);
     data.push_back(5);
     data.push_back(6);
+
+    vector<int> inner1;
+    inner1.push_back(1);
+    inner1.push_back(2);
     
-    vector<vector<int> > edges ={{1, 2}, {2,3}, {3,1}};
+    vector<int> inner2;
+    inner2.push_back(2);
+    inner2.push_back(3);
+
+    vector<int> inner3;
+    inner3.push_back(3);
+    inner3.push_back(1);
+
+    vector<vector<int> > edges;
+    edges.push_back(inner1);
+    edges.push_back(inner2);
+    edges.push_back(inner3);
+
+
     Graph<int, int> g=Graph<int, int>(keys, data, edges);
 }
