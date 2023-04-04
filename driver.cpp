f50199763fe183df85416bd54acfbd6e2ac339d9
anniepfeufer <pfeufe_a1@denison.edu>
2023-04-04 12:18:32 -0400

    update

--- driver.cpp
+++ driver.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "graph.h"
+#include "vector"
 using namespace std;
 
 int main(void)
 {
-    vector<int> keys = {1, 2, 3};
-    vector<int> data = {1, 2, 3};
-    vector<vector<int>> = {{1, 2}}
-    Graph(keys, vector<D> data, vector<vector<K>> edges)
+    vector<int> keys;
+    keys.push_back(1);
+    keys.push_back(2);
+    keys.push_back(3);
+
+    vector<int> data;
+    data.push_back(4);
+    data.push_back(5);
+    data.push_back(6);
+    
+    vector<vector<int> > edges ={{1, 2}, {2,3}, {3,1}};
+    Graph<int, int> g=Graph<int, int>(keys, data, edges);
 }
--- graph.h
+++ graph.h
@@ -16,24 +16,24 @@ private:
 	{
 		D data;
 		K key;
-	}
+	};
 
 	Node* V; //dynamic array of vertices 
 	int size; //number of verticies in the graph
 	vector<K>* Adj; //dynamic array of pointer to nodes 
 
 public:
-	Graph(vector<K> keys, vector<D> data, vector<vector<K>> edges){
+	Graph(vector<K> keys, vector<D> data, vector<vector<K> > edges){
 		size=size(keys);
-		V= new Node<D,K>[size]
-		Adj=new vector<K>[size]
+		V= new Node[size];
+		Adj=new vector<K>[size];
 		for (int i=0; i<size; i++){
-			Node<D,K> curr;
+			Node curr;
 			curr.data=data.at(i);
 			curr.key=keys.at(i);
 			V[i]=curr;
 			Adj[i]= edges.at(i);
 		}
 	}
-	Node<D,K>* get(K k);
+	Node* get(K k);
 };
