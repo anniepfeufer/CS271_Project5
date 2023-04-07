dd3ac97c387ef78a5816eac3367fcd57324bb529
anniepfeufer <pfeufe_a1@denison.edu>
2023-04-07 11:15:29 -0400

    test

--- test_graph.cpp
+++ test_graph.cpp
@@ -4,6 +4,39 @@
 #include <sstream>
 #include "graph.cpp"
 
+Graph<int, int>* generate_graph(){
+    vector<int> keys;
+    keys.push_back(1);
+    keys.push_back(2);
+    keys.push_back(3);
+
+    vector<int> data;
+    data.push_back(4);
+    data.push_back(5);
+    data.push_back(6);
+
+    vector<int> inner1;
+    inner1.push_back(1);
+    inner1.push_back(2);
+    vector<int> inner2;
+    inner2.push_back(2);
+    inner2.push_back(3);
+    vector<int> inner3;
+    inner3.push_back(3);
+    inner3.push_back(1);
+
+    vector<vector<int> > edges;
+    edges.push_back(inner1);
+    edges.push_back(inner2);
+    edges.push_back(inner3);
+    
+    Graph<int, int>* g= new Graph<int, int>(keys, data, edges);
+    return g;
+}
+
+
 int main(){
+    Graph<int, int>* g=generate_graph();
+    delete g;
     return 0;
 }
